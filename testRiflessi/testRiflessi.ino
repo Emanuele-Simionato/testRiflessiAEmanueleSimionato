82cc10fc062320f56821ad802aff6d36155eee76
Emanuele <emanuele.simionato@barsanti.edu.it>
2019-03-02 11:50:03 +0100

    Test componenti
    Manca solo il controllo bottone

--- testRiflessi/testRiflessi.ino
+++ testRiflessi/testRiflessi.ino
@@ -1,9 +1,27 @@
+#include<LiquidCrystal.h>
+LiquidCrystal LCD(12,11,5,4,3,2);
+int LED = 7;
+int BUZZ = 8;
+int BUTTON = 13;
 void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZ, OUTPUT);
+  pinMode(BUTTON, OUTPUT);
   // put your setup code here, to run once:
-
+LCD.begin(16,2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+LCD.print("Ciao");
+delay(1000);
+LCD.clear();
+LCD.print("Mondo");
+delay(1000);
+LCD.clear();
+tone(BUZZ, 1000, 200);
+digitalWrite(LED, HIGH);
+delay(1000);
+digitalWrite(LED, LOW);
+delay(1000);
 }
