a4d17d5df13e11fb1cbbdbbd4f13957770e35858
Emanuele <emanuele.simionato@barsanti.edu.it>
2019-03-08 10:56:23 +0100

    prima scrittura del programma

--- testRiflessi/testRiflessi.ino
+++ testRiflessi/testRiflessi.ino
@@ -3,25 +3,59 @@ LiquidCrystal LCD(12,11,5,4,3,2);
 int LED = 7;
 int BUZZ = 8;
 int BUTTON = 13;
+int v = 0;
+int k = 0;
+int p = 0;
 void setup() {
   pinMode(LED, OUTPUT);
   pinMode(BUZZ, OUTPUT);
   pinMode(BUTTON, OUTPUT);
   // put your setup code here, to run once:
 LCD.begin(16,2);
+randomSeed(analogRead(0));
 }
-
+void start(void){
+  v = digitalRead(BUTTON);
+while(v == LOW)
+{
+  LCD.print("Premere il pulsante per iniziare");
+  delay(500);
+  LCD.clear();
+  delay(500);
+}
+if(v == HIGH)
+{
+  LCD.clear();
+  LCD.print("Test iniziato");
+}
+LCD.clear();
+delay(1000);
+LCD.print("3");
+LCD.clear();
+delay(1000);
+LCD.print("2");
+LCD.clear();
+delay(1000);
+LCD.print("1");
+LCD.clear();
+  }
+void accendi(void){
+  digitalWrite(LED,HIGH);
+  k = digitalRead(BUTTON);
+  while(k == LOW){
+    digitalWrite(LED,HIGH);
+  }
+  if(k == HIGH){
+    digitalWrite(LED,LOW);
+  }
+}
+void suona(void){
+ 
+}
 void loop() {
   // put your main code here, to run repeatedly:
-LCD.print("Ciao");
-delay(1000);
-LCD.clear();
-LCD.print("Mondo");
-delay(1000);
-LCD.clear();
-tone(BUZZ, 1000, 200);
-digitalWrite(LED, HIGH);
-delay(1000);
-digitalWrite(LED, LOW);
-delay(1000);
+start();
+delay(random(1000,5000));
+accendi();
+delay(random(1000,5000));
 }
