5588e0e87d0e9963666ade7f2649af68f2341c6a
Emanuele <emanuele.simionato@barsanti.edu.it>
2019-03-23 10:19:30 +0100

    commenti aggiunti

--- testRiflessi/testRiflessi.ino
+++ testRiflessi/testRiflessi.ino
@@ -1,12 +1,12 @@
-#include<LiquidCrystal.h>
-LiquidCrystal LCD(12,11,5,4,3,2);
-int G = 7;
-int B = 13;
-int R = 6;
-int BUZZ = 10;
-int BUTTON = 8;
-int tempoL = 0;
-int tempoB = 0;
+#include<LiquidCrystal.h>//richiamo libreria LCD
+LiquidCrystal LCD(12,11,5,4,3,2);//pin schermo LCD
+int G = 7;//pin colore verde
+int B = 13;//pin colore blu
+int R = 6;//pin colore roso
+int BUZZ = 10;//pin buzzer
+int BUTTON = 8;//pin pulsante
+int tempoL = 0;//variabile tempo risposta del led
+int tempoB = 0;//variabile tempo risposta del buzzer
 void setup() {
   pinMode(G, OUTPUT);
   pinMode(B, OUTPUT);
@@ -14,10 +14,10 @@ void setup() {
   pinMode(BUZZ, OUTPUT);
   pinMode(BUTTON, INPUT);
   // put your setup code here, to run once:
-LCD.begin(16,2);
+LCD.begin(16,2);//inizializzo lo schermo LCD
 randomSeed(analogRead(0));
 }
-void start(){
+void start(){//metodo che attende l'imput dall'utente perchè inizi il programma
   LCD.print("Premi il bottone");
   LCD.setCursor(0,1);
   LCD.print("per iniziare");
@@ -28,7 +28,7 @@ while(digitalRead(BUTTON) == LOW){}
   LCD.clear();
   LCD.setCursor(1,0);
 }
-void media(){
+void media(){//metodo che calcola il tempo medio di risposta e determina se il test è stato superato dall'utente
   int media = (tempoL+tempoB)/2;
   LCD.print("Tempo medio:");
   delay(1000);
@@ -58,7 +58,7 @@ void media(){
     digitalWrite(R, LOW);
   }
 }
-void conta(){
+void conta(){//conto alla rovescia prima dell'inizio del programma
   LCD.print("3");
   delay(1000);
   LCD.clear();
@@ -69,7 +69,7 @@ void conta(){
   delay(1000);
   LCD.clear();
 }
-void accendi(){
+void accendi(){//funzione che accende il led e calcola il tempo di risposta dell'utente
   int a = millis();
   digitalWrite(B,HIGH);
   while(digitalRead(BUTTON)== LOW){}
@@ -84,7 +84,7 @@ void accendi(){
     LCD.clear();
   }
 }
-void suona(){
+void suona(){//funzione che attiva il buzzer e calcola il tempo di risposta dell'utente
   tone(BUZZ, 1000);
   int t = millis();
   while(digitalRead(BUTTON) == LOW){}
